78e65435275614c2a2c4d25cdc4a1ff55e51ee0e
Colin Quelle <cmq1999@yahoo.com>
2024-02-10 19:27:58 -0500

    server pinged every 14 minutes

--- backend/src/ping.js
+++ backend/src/ping.js
@@ -3,7 +3,7 @@ const https = require('https');
 require('dotenv').config();
 
 const backendUrl = process.env.SERVER_URL;
-const ping = new cron.CronJob('*/1 * * * *', function () {
+const ping = new cron.CronJob('*/14 * * * *', function () {
     console.log('pinging server');
 
     https.get(backendUrl, (res) => {
